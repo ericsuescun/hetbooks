0dc8f21099ad4e8245e0f85bdf9a8e0bee0e4c90
Eric Suescun <ericsuescun@MacBook-Air-de-Eric.local>
2021-01-04 15:05:23 -0500

    Se elimina el merge de user id en el book controller

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,7 @@ class Book < ApplicationRecord
 
 	def self.import(file)
 		CSV.foreach(file.path, :col_sep => (";"), headers: true, encoding: 'iso-8859-1:utf-8') do |row|
-			Book.create!(row.to_hash.merge(user_id: 1))
+			Book.create!(row.to_hash)
 		end
 	end
 
